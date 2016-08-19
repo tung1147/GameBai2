f7a96d5f0185c3617ba512d98afa22bbd1d3cf2f
Quyet Nguyen <quyetnd@puppetteam.com>
2016-08-19 15:02:17 +0700

    len la len la len

---
+++ res/Game/js/SceneNavigator.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Quyet Nguyen on 8/9/2016.
+ */
+
+var SceneNavigator = SceneNavigator || {};
+SceneNavigator.toHome = function () {
+
+};
+
+SceneNavigator.toGame = function () {
+
+};
+
+SceneNavigator.toLobby = function (gameId) {
+
+};
+
+SceneNavigator.toGame = function (gameId) {
+
+};
