48a6ccbe20e2add4a1a6afa9a72ed1a728e39405
QuyetNguyen <dangquyet1601@gmail.com>
2016-09-03 21:29:21 +0700

    fix loading image

--- frameworks/runtime-src/proj.ios_mac/ios/iOS_native_linker.mm
+++ frameworks/runtime-src/proj.ios_mac/ios/iOS_native_linker.mm
@@ -12,14 +12,13 @@
 #import <StoreKit/StoreKit.h>
 
 void c_to_objC_callSupport(const char* numSupport){
-    NSString *phNo = [NSString stringWithCString: numSupport encoding:NSUTF8StringEncoding];
-    NSURL *phoneUrl = [NSURL URLWithString:[NSString  stringWithFormat:@"telprompt:%@",phNo]];
-    
+    NSString *phoneNum = [NSString stringWithCString: numSupport encoding:NSUTF8StringEncoding];
+    NSURL *phoneUrl = [NSURL URLWithString:[NSString  stringWithFormat:@"telprompt:%@", phoneNum]];
     if ([[UIApplication sharedApplication] canOpenURL:phoneUrl]) {
         [[UIApplication sharedApplication] openURL:phoneUrl];
-    } else
-    {
-        UIAlertView  *calert = [[UIAlertView alloc]initWithTitle:@"Alert" message:@"Call facility is not available!!!" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
+    } else{
+        NSString* message = [NSString stringWithFormat:@"Thiết bị không hỗ trợ gọi điện, vui lòng gọi %@ để được hỗ trợ", phoneNum];
+        UIAlertView  *calert = [[UIAlertView alloc]initWithTitle:@"Hỗ trợ" message:message delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
         [calert show];
     }
 }
@@ -71,7 +70,7 @@ void c_to_objC_buyItem(const std::string& item){
 }
 
 void c_to_objC_set_iClound_no_backup_folder(const char* folderPath){
-    NSString* folder = [[NSString alloc] initWithUTF8String:folderPath];
+    NSString* folder = [NSString stringWithUTF8String:folderPath];
     NSURL* URL= [NSURL fileURLWithPath: folder];
     if([[NSFileManager defaultManager] fileExistsAtPath:[URL path]] == NO){
          NSLog(@"Folder %@ not found", [URL path]);
