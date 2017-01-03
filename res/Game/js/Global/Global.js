95e5d27ed051132e1c114cc085dfef335a738d70
Quyet Nguyen <dangquyet1601@gmail.com>
2017-01-03 16:41:20 +0700

    email

--- res/Game/js/Global/Global.js
+++ res/Game/js/Global/Global.js
@@ -178,7 +178,7 @@ PlayerMe.messageCount = 0;
 PlayerMe.SFS = PlayerMe.SFS || {};
 
 var GameConfig = GameConfig || {};
-GameConfig.email = "gamebai_c567@gmail.com";
+GameConfig.email = "c567vip@gmail.com";
 GameConfig.hotline = "09000123";
 GameConfig.broadcastMessage = "";
 GameConfig.DeviceIDKey = "";
