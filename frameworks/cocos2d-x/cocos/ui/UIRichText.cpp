c69930919d4e10dfb384e9447ff28f830780a484
Quyet Nguyen <dangquyet1601@gmail.com>
2017-01-17 17:48:52 +0700

    RichText

--- frameworks/cocos2d-x/cocos/ui/UIRichText.cpp
+++ frameworks/cocos2d-x/cocos/ui/UIRichText.cpp
@@ -1765,6 +1765,8 @@ void RichText::formarRenderers()
             maxHeights[i] = maxHeight;
             newContentSizeHeight += maxHeights[i];
         }
+		/*mod by quyetnd*/
+		_customSize.height = newContentSizeHeight;
         
         float nextPosY = _customSize.height;
         for (size_t i=0, size = _elementRenders.size(); i<size; i++)
