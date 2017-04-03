38843bffb6b90600a120cae980e54dc83aec1bdf
Quyet Nguyen <dangquyet1601@gmail.com>
2017-04-03 16:54:08 +0700

    ChatDialog

--- res/Game/js/Dialog/ChatDialog.js
+++ res/Game/js/Dialog/ChatDialog.js
@@ -13,37 +13,86 @@ var s_chat_message = s_chat_message || [
 var ChatDialog = IDialog.extend({
     ctor : function () {
         this._super();
-        this.setContentSize(cc.winSize);
+        var thiz = this;
+        this.mTouch = cc.rect(0, 226, 958, 494);
+
+        this.setContentSize(cc.size(958, cc.winSize.height));
+
+        var bg = new ccui.Scale9Sprite("board_bg.png", cc.rect(105, 105, 147, 147));
+        bg.setPreferredSize(cc.size(1154,700));
+        this.addChild(bg);
+        bg.setPosition(this.getContentSize().width/2, 480);
+
+        var closeButton = new ccui.Button("chat_close_bt.png", "", "", ccui.Widget.PLIST_TEXTURE);
+        closeButton.setPosition(this.getContentSize().width/2, 230);
+        this.addChild(closeButton);
+        closeButton.addClickEventListener(function () {
+             thiz.hide();
+        });
+
         this.initAllChat();
+        this.initAllEmotion();
     },
     initAllChat : function () {
-        var col = 3;
-        var row = Math.ceil(s_chat_message.length/col);
+        var right = 539;
+        var left = 0;
+        var top = 720;
+        var bottom = 265;
 
-        var padding = 40.0;
-        var itemWidth = (cc.winSize.width - padding*(col+1)) / col;
-        var itemHeight = 100.0;
+        var listMessage = new newui.TableView(cc.size(right - left, (top - bottom)), 2);
+        listMessage.setPadding(30);
+        listMessage.setBounceEnabled(true);
+        listMessage.setMargin(30,30,0,0);
+        listMessage.setScrollBarEnabled(false);
+        listMessage.setPosition(left, bottom);
+        this.addChild(listMessage);
 
         var thiz = this;
         for(var i=0;i<s_chat_message.length;i++){
-            var x = padding + itemWidth/2 + (itemWidth +padding) * (i%col);
-            var y = cc.winSize.height/2 - itemHeight*(row-1) / 2 + itemHeight *Math.floor(i/col);
-
             var bg = new ccui.Button("dialog_chat_bg.png", "","",ccui.Widget.PLIST_TEXTURE);
             bg.setCapInsetsNormalRenderer(cc.rect(50,0,4,60));
             bg.setScale9Enabled(true);
-            bg.setContentSize(cc.size(itemWidth, 60));
-            bg.setPosition(x,y);
+            bg.setContentSize(cc.size(240, 60));
             bg.setZoomScale(0.02);
-            this.addChild(bg);
+            listMessage.pushItem(bg);
 
-            var message = new cc.LabelBMFont(s_chat_message[i], cc.res.font.Roboto_Condensed_25);
+            var message = new cc.Label.createWithBMFont(cc.res.font.Roboto_Condensed_18, s_chat_message[i]);
             message.setPosition(bg.getContentSize().width/2, bg.getContentSize().height/2);
             bg.getRendererNormal().addChild(message);
             (function () {
                 var msg = s_chat_message[i];
                 bg.addClickEventListener(function () {
                     thiz._buttonHandler(msg);
+                });
+            })();
+        }
+    },
+
+    initAllEmotion : function () {
+        var right = 958;
+        var left = 539;
+        var top = 720;
+        var bottom = 265;
+
+        var listMessage = new newui.TableView(cc.size(right - left, (top - bottom)), 5);
+        listMessage.setPadding(20);
+        listMessage.setBounceEnabled(true);
+        listMessage.setMargin(30,30,0,0);
+        listMessage.setScrollBarEnabled(false);
+        listMessage.setPosition(left, bottom);
+        this.addChild(listMessage);
+
+        var thiz = this;
+        for(var i=0;i<40;i++){
+            var icon = new ccui.Button("emotion_1.png", "","",ccui.Widget.PLIST_TEXTURE);
+            icon.setZoomScale(0.02);
+            listMessage.pushItem(icon);
+
+            (function () {
+               // var msg = s_chat_message[i];
+                icon.addClickEventListener(function () {
+                   // thiz._buttonHandler(msg);
+                    thiz.hide();
                 });
             })();
         }
