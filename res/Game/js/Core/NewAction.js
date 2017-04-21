7a664b1e118309743d30a396c61f1a4de0fe832c
Quyet Nguyen <dangquyet1601@gmail.com>
2017-04-21 16:32:09 +0700

    Fix

--- res/Game/js/Core/NewAction.js
+++ res/Game/js/Core/NewAction.js
@@ -7,20 +7,26 @@ var quyetnd = quyetnd || {};
 quyetnd.ActionShake2D = cc.CustomAction.extend({
     ctor : function (duration, strength) {
         this._super();
-        this._strength = null;
+        this.initWithDuration(duration);
+        this._strength = cc.p(strength);
         this._originPosition = null;
         this._target = null;
-
-        this._super();
-        this.initWithDuration(duration);
-        this._strength = strength;
+        
     },
 
     onStop : function () {
-        this._target.setPosition(this._originPosition);
+        if(this._target && this._originPosition){
+            this._target.setPosition(this._originPosition);
+        }
     },
 
     onUpdate : function (dt) {
+        if(!this._target){
+            return;
+        }
+        if(!this._originPosition){
+            this._originPosition = this._target.getPosition();
+        }
         this._target.x = this._originPosition.x + this._strength.x - (Math.random() * this._strength.x * 2);
         this._target.y = this._originPosition.y + this._strength.y - (Math.random() * this._strength.y * 2);
     },
