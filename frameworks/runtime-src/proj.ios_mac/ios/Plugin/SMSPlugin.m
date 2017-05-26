857a068c9834a385e627383f443951239d29141e
Quyet Nguyen <dangquyet1601@gmail.com>
2017-05-26 11:29:40 +0700

    fix sendSMS ios

--- frameworks/runtime-src/proj.ios_mac/ios/Plugin/SMSPlugin.m
+++ frameworks/runtime-src/proj.ios_mac/ios/Plugin/SMSPlugin.m
@@ -1,6 +1,5 @@
 #import <Foundation/Foundation.h>
 #import "SMSPlugin.h"
-#import <MessageUI/MessageUI.h>
 
 
 @implementation SMSPlugin
@@ -27,13 +26,35 @@ static SMSPlugin* s_SMSPlugin = 0;
     NSArray *recipents = @[numberPhone];
     
     MFMessageComposeViewController *messageController = [[MFMessageComposeViewController alloc] init];
-   // messageController.messageComposeDelegate = self;
+    messageController.messageComposeDelegate = self;
     [messageController setRecipients:recipents];
     [messageController setBody:content];
     
     // Present message view controller on screen
     [controller presentViewController:messageController animated:YES completion:nil];
     return YES;
+}
+
+- (void)messageComposeViewController:(MFMessageComposeViewController *)_controller didFinishWithResult:(MessageComposeResult)result{
+//    switch (result) {
+//        case MessageComposeResultCancelled:
+//            break;
+//            
+//        case MessageComposeResultFailed:
+//        {
+//            UIAlertView *warningAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Failed to send SMS!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+//            [warningAlert show];
+//            break;
+//        }
+//            
+//        case MessageComposeResultSent:
+//            break;
+//            
+//        default:
+//            break;
+//    }
+    
+    [_controller dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (id) initWithView:(UIViewController*) view {
